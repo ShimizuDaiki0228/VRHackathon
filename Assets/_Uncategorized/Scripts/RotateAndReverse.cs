a782951f077b6c59a003105d49af4c5cff4e8360
btakashi <>
2023-12-13 18:19:26 +0900

    スマートボール盤面　作業進捗を一旦コミット

---
+++ Assets/_Uncategorized/Scripts/RotateAndReverse.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class RotateAndReverse : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            RotateAndReturn();
+        }
+    }
+
+    public void RotateAndReturn()
+    {
+        // コルーチンを開始
+        StartCoroutine(RotateCoroutine());
+    }
+
+    private IEnumerator RotateCoroutine()
+    {
+        // 回転の速さや角度は必要に応じて調整
+        float rotationSpeed = 20f; // 1秒間に180度回転
+        float maxRotation = 105f;   // 最大回転角度
+        float returnRotation = 90f; // 戻る角度
+
+        float currentRotation = transform.rotation.eulerAngles.z;
+        float targetRotation = currentRotation + maxRotation;
+
+        // 大きく回転
+        while (currentRotation < targetRotation)
+        {
+            currentRotation += rotationSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0f, 0f, currentRotation);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(.1f);
+
+        // 少し過ぎてから戻す（わずかな遊びを設ける）
+        float overshootRotation = currentRotation - returnRotation;
+        while (overshootRotation < 0f)
+        {
+            currentRotation += rotationSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0f, 0f, currentRotation);
+            overshootRotation = currentRotation - returnRotation;
+            yield return null;
+        }
+
+        // 最終的に-90度まで戻す
+        transform.rotation = Quaternion.Euler(0f, 0f, returnRotation);
+    }
+}
